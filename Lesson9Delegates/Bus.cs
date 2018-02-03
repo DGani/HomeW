a1cf12014984b16ad07d2e0cd176c99f9b14c8db
DGani <33170747+DGani@users.noreply.github.com>
2018-02-03 23:39:40 +0600

    Добавьте файлы проекта.

---
+++ Lesson9Delegates/Bus.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9Delegates
+{
+    class Bus:Car
+    {
+        int Finish = 10000;
+        int Mills = 0;
+        int Time = 200;
+        public override int RacingAuto()
+        {
+            base.RacingAuto();
+            return SpeedAuto;
+        }
+        public override void Winner()
+        {
+            Console.WriteLine("You are winner!!!");
+            Console.WriteLine($"Last Moment Speed: {SpeedAuto}");
+            Console.WriteLine($"Mills: {Mills}");
+            Console.WriteLine($"Time : {Time}");
+        }
+        public int GetFinish()
+        {
+
+            while (Time > 0)
+            {
+
+
+                if (RacingAuto() > 0 && SpeedAuto < 150)
+                {
+
+                    Mills = Mills + SpeedAuto;
+                    Time--;
+                    Console.WriteLine(Time);
+
+                }
+                if (RacingAuto() > 150)
+                {
+
+                    Random rnd = new Random();
+                    SpeedAuto = SpeedAuto - RacingAuto();
+                    Time--;
+                    Console.WriteLine(Time);
+                }
+                if (RacingAuto() < 0)
+                {
+                    Console.WriteLine("Stop"); SpeedAuto = 0;
+                    Time--;
+                    Console.WriteLine(Time);
+                }
+                if (Mills > Finish)
+                {
+                   
+                    break;
+                }
+                if (Time == 0)
+                {
+                    Console.WriteLine("Время вышло!");
+                }
+
+            }
+
+
+
+            return Mills;
+        }
+        
+    }
+}
---
+++ Lesson9Delegates/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson9Delegates
+{
+    class Program
+    {
+       
+        
+
+        static void Main(string[] args)
+        {
+            SportCar formula1 = new SportCar();
+            formula1.NameAuto = "Mitshubishi";
+            PassengerCar Vanya = new PassengerCar();
+            Vanya.NameAuto = "Toyota";
+            Car.ShowCharacters method = Show;
+            formula1.Eventt += formula1.Winner;
+            Vanya.Eventt += Vanya.Winner;
+            while (true)
+            {
+               formula1.RacingAuto();
+                Vanya.RacingAuto();
+             if (formula1.GetFinish() > 10000)
+                {
+                    Console.WriteLine("Winner Formula 1");
+                    //formula1.Eventt; не смог(
+                    break;              
+                }
+                if (Vanya.GetFinish() > 10000)
+                {
+                    Console.WriteLine("Winner Vasya");
+                   // Vanya.Eventt;
+                    break;
+                  
+                }
+            }
+            Console.WriteLine("-------------------");
+            Console.Write("Марка первого авто: ");
+            formula1.Characters(method);
+            Console.Write("Марка второго авто: ");
+            Vanya.Characters(method);
+            Console.ReadLine();
+        }
+        static void Show(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
